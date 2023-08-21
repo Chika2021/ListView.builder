c96391b7c82618195befa59b34bc17fb69ecec17
Chika2021 <amaechichika9@gmail.com>
2023-08-21 09:39:30 +0100

    Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -22,90 +22,72 @@ class Dynamic extends StatefulWidget {
 }
 
 class _DynamicState extends State<Dynamic> {
-  List<String> contact = ['Herald', 'Joffry', 'Amber','Herald', 'Joffry', 'Amber'];
-  List<String> selected = [];
-
-  void contactSelected(person) {
-    setState(() {
-      selected.add(person);
-    });
-  }
-
+  final List<String> _post = [
+    'Herald',
+    'Joffry',
+    'Amber',
+    'Herald',
+    'Joffry',
+    'Amber',
+    'Revelation',
+    'Rolex'
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.grey[300],
-        // appBar: AppBar(title: const Text('Project Final')),
-        body:
-            // Center(
-            //   child: Container(
+        // appBar: AppBar(title: const Center( child:Text('Welcome'))),
+        body: Column(
+      children: [
+        Row(
+          children: [
+            Container(
+              height: 100,
+              width: 100,
+              // color: Colors.pink,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.pink
+              ),
+            ),
+             Container(
+              height: 100,
+              width: 100,
+              // color: Colors.pink,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.pink
+              ),
+            ),
+             Container(
+              height: 100,
+              width: 100,
+              // color: Colors.pink,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.pink
+              ),
+            ),
+            Container(
+              height: 100,
+              width: 100,
+              // color: Colors.pink,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.pink
+              ),
+            ),
+            
+          ],
+        ),
+        Expanded(
+            child:ListView.builder(
+            itemCount: _post.length,
+            itemBuilder: (context, index) {
+              return Square(child: _post[index]);
+            })
+           )
 
-            //   height: 200 ,
-            //   width: 200,
-
-            //   decoration: BoxDecoration(
-            //       shape: BoxShape.circle,
-            //       color:Colors.grey[300],
-            //       boxShadow: [
-            //         BoxShadow(
-            //           color: Colors.grey.shade500,
-            //           offset: Offset(4.0, 4.0),
-            //           blurRadius: 15.0,
-            //           spreadRadius: 1.0
-            //         ),
-
-            //       ]
-
-            //     )
-            //   Column(
-            // children: [
-            // Expanded(
-            //   flex: 15,
-            //   child:Container(height: 100, color: Colors.deepPurple),
-            // ),
-            // Expanded(
-            //   flex: 5,
-            //   child:  Container(height: 100, color: Colors.pink,),
-            //   ),
-            // Expanded(
-            //   flex:8,
-            //   child: Container(height: 100, color: Colors. blue),
-            //   )
-            //     ],
-            // ),
-
-            // ListView(
-
-            //   children: [
-            //     Square(),
-            //     Square(),
-            //     Square(),
-            //     Square(),
-            //     Square(),
-            //     Square(),
-            //     Square(),
-            //     Square(),
-            //     Square(),
-            //     Square(),
-            //     Square(),
-            //     Square(),
-            //     Square(),
-            //     Square(),
-            //     Square(),
-            //     Square(),
-            //     Square(),
- 
-            //   ],
-            // ),
-
-            //Dynamically Create a ListView
-
-            ListView.builder(
-                itemCount: contact.length,
-                itemBuilder: (context, index) {
-                  return Square(child: contact[index]);
-                }));
-
-    // decoration: BoxDecoration(borderRadius: BorderRadius.circular(15)),
+      ],
+    ));
   }
 }
